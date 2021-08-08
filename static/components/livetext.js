7432537ea69f9ee0373e7889bceaf654b684c153
Gurleen Singh <gs585@drexel.edu>
2021-08-08 19:24:47 -0400

    Fixed fade tween disappearing

--- static/components/livetext.js
+++ static/components/livetext.js
@@ -9,16 +9,18 @@ class LiveText extends HTMLElement {
         let key = this.getAttribute("key");
         this.willFade = this.getAttribute("fade") === "true";
         LiveText.liveTextObjects[key] = this;
+        if(this.willFade) {
+            this.tween = gsap.to(this, {opacity: 0, duration: 0.1});
+        }
     }
 
     static update(key, value) {
         if(!(key in this.liveTextObjects)) { return; }
         let element = this.liveTextObjects[key];
         if(element.willFade) {
-            let tween = gsap.to(element, {opacity: 0, duration: 0.1});
-            tween.play().then(() => {
+            element.tween.play().then(() => {
                 element.innerText = value;
-                tween.reverse();
+                element.tween.reverse();
             })
         } else {
             element.innerText = value;
