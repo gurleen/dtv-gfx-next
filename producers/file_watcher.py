b23ac46bee7ae3fb2eb277606168e6957574bde9
Gurleen Singh <gs585@drexel.edu>
2021-08-12 01:23:21 -0400

    Added cpu-bound process_file function

--- producers/file_watcher.py
+++ producers/file_watcher.py
@@ -1,11 +1,21 @@
+import time
 import asyncio
+import concurrent.futures
 import aiofiles
 from watchgod import awatch
 
 
+FILE_NAME = "/srv/ftp/test"
+
+
 async def watch_file(q: asyncio.Queue):
+    loop = asyncio.get_event_loop()
     async for changes in awatch("/srv/ftp/test"):
         print(changes)
-        async with aiofiles.open("/srv/ftp/test", mode="r") as f:
-            async for line in f:
-                print(line)
+        result = await loop.run_in_executor(None, process_file)
+        print(result)
+
+
+def process_file():
+    time.sleep(5)
+    return "hello!"
